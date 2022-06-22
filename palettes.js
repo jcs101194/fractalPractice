0ac658d073de21bbce5a2bd521f4622d9c9c8a96
jcs101194 <jcs101194@yahoo.com>
2022-06-22 11:36:33 -0500

    improved camera; sphereflake bug fix; other changes

---
+++ palettes.js
@@ -0,0 +1,27 @@
+class Pastel
+{
+	constructor()
+	{
+		this.BACKGROUND = 0x97a2ff;
+
+		this.FOG = 0x2b0761;
+
+		this.COLOR_1 = 0x11e8bb;
+		this.COLOR_2 = 0xfcc2ff;
+		this.COLOR_3 = 0x8200C9;
+	}
+}
+
+class Theme2
+{
+	constructor()
+	{
+		this.BACKGROUND = 0x97a2ff;
+
+		this.FOG = 0x2b0761;
+
+		this.COLOR_1 = 0x11e8bb;
+		this.COLOR_2 = 0xfcc2ff;
+		this.COLOR_3 = 0x8200C9;
+	}
+}

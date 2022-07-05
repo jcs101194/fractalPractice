08984aced5129946fa43e97a002973a1e447cd62
jcs101194 <jcs101194@yahoo.com>
2022-07-05 01:50:21 -0500

    improved cameras, menger sponge, spherical camera bug fix

--- palettes.js
+++ palettes.js
@@ -12,16 +12,16 @@ class Pastel
 	}
 }
 
-class Theme2
+class Duststorm
 {
 	constructor()
 	{
-		this.BACKGROUND = 0x97a2ff;
+		this.BACKGROUND = 0xAD836A;
 
-		this.FOG = 0x2b0761;
+		this.FOG = 0x9E7154;
 
-		this.COLOR_1 = 0x11e8bb;
-		this.COLOR_2 = 0xfcc2ff;
+		this.COLOR_1 = 0x733A26;
+		this.COLOR_2 = 0xd4781c;
 		this.COLOR_3 = 0x8200C9;
 	}
 }
